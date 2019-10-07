436106af040eadfa2258809b90e3849c1698a41b
apsteinmetz <apsteinmetz@yahoo.com>
2019-10-07 17:52:32 -0400

    parsing done.  as good as it's gonna get.

--- parse_residents.r
+++ parse_residents.r
@@ -5,12 +5,13 @@ library(tidyverse)
 # change periods in dates to dashes and deliniate
 #accounts for "i" in place if "1" in some years
 # vectorize so we can use in dplyr::mutate
-date_str <-"([0-9]{1,2}(\\.|:) {0,2}[0-9]{0,2}\\. {0,2}([0-9]{2,4}|i[0-9]{3}))|([0-9]{4}|i[0-9]{3})"
+date_str <-"([0-9]{1,2}(\\.|:|,) {0,2}[0-9]{0,2}\\. {0,2}([0-9]{2,4}|i[0-9]{3}))|([0-9]{4}|i[0-9]{3})"
 #date_str_2 <-"([0-9]{1,2}(\\.|:) {0,2}[0-9]{0,2}\\. {0,2}([0-9]{2,4}|i[0-9]{3}))"
 make_yeardate <- Vectorize(function(orig_str){
   if (!is.na(orig_str)){
     if (str_detect(orig_str,date_str)){
       temp_str <- str_extract(orig_str,date_str)
+      temp_str <- str_replace_all(temp_str,"i","1")
       temp_str <- paste0("ddd",str_replace_all(temp_str,"\\. {0,2}","-"),"ddd")
       orig_str <- str_replace(orig_str,date_str,temp_str)
     }
@@ -55,16 +56,17 @@ raw_data_1 <- raw_data %>%
   mutate(value = str_replace_all(value,"ev {0,1}(\\.|,)","Evangelical")) %>% 
   mutate(value = str_replace_all(value,"ref {0,1}\\.","Reformed")) %>%
   # places
-  mutate(value = str_replace_all(value,"Krs|Kr {0,1}\\.","Krs")) %>%
+  mutate(value = str_replace_all(value,"(Krs|Kr) {0,1}\\.","Krs")) %>%
   mutate(value = str_replace_all(value,"Jug {0,1}\\.","Jugoslavia")) %>%
   mutate(value = str_replace_all(value,"†","died")) %>% 
   mutate(value = str_replace_all(value,", {0,2}sen\\."," Senior")) %>% 
   # fates
   # questions: is "gef." "geßtorben" or "gefallen" (which would be "killed in action")? I translate to "died."
-  mutate(value = str_replace_all(value,"gef {0,1}\\.","died")) %>%
-  mutate(value = str_replace_all(value,"gefallen","died")) %>%
+  mutate(value = str_replace_all(value,"gef {0,1}(\\.|,)","died")) %>%
+  mutate(value = str_replace_all(value,"gefallen","killed in action")) %>%
   mutate(value = str_replace_all(value,"vermißt","missing")) %>% 
   mutate(value = str_replace_all(value,"ermordet","murdered")) %>% 
+  mutate(value = str_replace_all(value,"ermordert","murdered")) %>% 
   mutate(value = str_replace_all(value,"erhängte","hanged")) %>%  
   mutate(value = str_replace_all(value,", {0,2}jun\\."," Junior")) %>% 
   mutate(value = str_replace_all(value,"r\\. {0,2}k\\.","Catholic")) %>% 
@@ -103,6 +105,14 @@ raw_data_2 <- raw_data_1 %>%
   mutate(value = ifelse(street %in% schowe_street_names,str_remove(value,address_str),value)) %>%
   {.}
 
+# extract fates
+raw_data_2$fate <- "unknown"
+for (n in 1:length(fates)){
+  raw_data_2 <- raw_data_2 %>% 
+    mutate(fate=ifelse(str_detect(value,fates[n]),fates[n],fate)) %>% 
+    mutate(value=str_remove(value,fates[n]))
+    {.}
+}
 # now convert the anything that looks like a date or into birth then death date
 # everthing to the left is going to be name
 raw_data_3 <- raw_data_2 %>% 
@@ -119,7 +129,9 @@ raw_data_3 <- raw_data_2 %>%
 
 
 
-# parse names and faith
+# parse names
+last_loc_token <- "([^[:alnum:]])([a-zA-Z ])+$"
+
 raw_data_4 <- raw_data_3 %>%   
  separate(name,
           into=c("last_name","first_name"),
@@ -127,41 +139,58 @@ raw_data_4 <- raw_data_3 %>%
           extra = "merge") %>%
   mutate(first_name=str_remove(first_name,", $")) %>% 
   separate(last_name, into=c("last_name","maiden_name"), sep = " nee ") %>% 
-  separate(value,into=c("faith","fate"),extra = "merge") %>% 
+  {.}
+# --------- MAKE 5 ---------------
+raw_data_5 <- raw_data_4 %>%
+  separate(value,into=c("faith","last_location_2"),extra = "merge") %>% 
+  # mutate(last_location = str_extract(last_location,last_loc_token)) %>% 
+  mutate(last_location_2 = ifelse(!(faith %in% faiths) & (is.na(last_location_2)), faith, last_location_2)) %>% 
+  mutate(last_location = ifelse(is.na(last_location) | last_location=="",last_location_2,last_location)) %>%
   {.}
 
 # Now it gets to be a tough slog.  Try to clean up some of remaining mess.  
-raw_data_5 <- raw_data_4 %>% 
-  mutate(fate = ifelse(!is.na(faith) & (is.na(fate) | fate ==""), faith, fate)) %>% 
-  mutate(last_location = ifelse(!is.na(faith) & (is.na(last_location) | last_location ==""),fate,last_location)) %>%
+raw_data_5 <- raw_data_5 %>% 
   separate(last_location,into = c("last_location","last_district"),sep = " Krs ",extra = "merge") %>%
   mutate(last_location=ifelse(str_detect(last_location,"Lager"),str_extract(last_location,"Lager [a-zA-Z]+"),last_location)) %>%
   mutate(faith = ifelse(faith == "e$|ev$","Evangelical",faith)) %>% 
-  mutate(fate=ifelse(!(faith %in% faiths),faith,fate)) %>%
   mutate(faith=ifelse(faith %in% faiths,faith,"unknown")) %>%
   select(-raw_text,everything()) %>% 
   {.}
 
-# pull remaining fates out of last_location
+
 for (n in 1:length(fates)){
   raw_data_5 <- raw_data_5 %>% 
+    mutate(fate=ifelse(str_detect(last_location_2,fates[n]),fates[n],fate)) %>% 
     mutate(fate=ifelse(str_detect(last_location,fates[n]),fates[n],fate)) %>% 
-    mutate(last_location=str_remove(last_location,fates[n]))
-  }
+    mutate(last_location=str_remove(last_location,fates[n])) %>% 
+    {.}
+}
 raw_data_5 <- raw_data_5 %>% 
-  mutate(fate=ifelse(fate %in% fates,fate,"unknown"))
-  
+  separate(last_location,into=c("last_location","last_location_2"),sep=",") %>%
+  mutate(last_district=ifelse(is.na(last_district),last_location_2,last_district)) %>% 
+  mutate(last_location=ifelse(str_detect(last_location,"[0-9]"),"unknown",last_location)) %>% 
+  mutate(faith=ifelse(str_detect(last_location,"^.v"),"Evangelical",faith)) %>% 
+  mutate(last_location=ifelse(str_detect(last_location,"^.v"),last_district,last_location)) %>% 
+  {.}
 
-prepositions <- c("^am ","^bei ","^nach ","^an ","^in ")
+raw_data_5 <- raw_data_5 %>% select(-last_location_2,everything())
+
+
+prepositions <- c("^am ","^bei ","^nach ","^an ","^in ","und$")
 raw_data_5$last_location <- trimws(raw_data_5$last_location)
 for (n in 1:length(prepositions)){
   raw_data_5 <- raw_data_5 %>% 
   mutate(last_location=str_remove(last_location,prepositions[n]))
 }
 
+
+# ------------- Make Final------------------
 # convert NAs to "unknown" as appropriate
 schowe_residents_1944 <- raw_data_5
 schowe_residents_1944[is.na(schowe_residents_1944)] <- "unknown"
 schowe_residents_1944[schowe_residents_1944 == ""] <- "unknown"
 # maiden names should be NA for men and unmarried women
 schowe_residents_1944$maiden_name[schowe_residents_1944$maiden_name == "unknown"] <- NA
+schowe_residents_1944 <- schowe_residents_1944 %>% 
+  select(last_name,first_name,maiden_name,faith,street,house,village,born,
+         died,fate,last_location,last_district,birth_date,death_date,raw_text)
